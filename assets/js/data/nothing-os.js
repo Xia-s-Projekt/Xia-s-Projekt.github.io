fd5b68c0ca49c83705505a882fde6fa7a9553357
ryanistr <ridho.holy@gmail.com>
2026-05-10 12:30:10 +0700

    refactor: standardize codebase and update Nothing OS assets

--- assets/js/data/nothing-os.js
+++ assets/js/data/nothing-os.js
@@ -2,12 +2,25 @@ OS_DATA.push({
   id: "nothing-os",
   name: "Nothing OS",
   hide: false,
-  image: "assets/images/banners/nothingos.jpg",
+  image: "assets/images/banners/nothingos.png",
   shortDesc: "Nothing OS port with dot-matrix design.",
   fullDesc: "A ported version of Nothing OS bringing the dot-matrix design and smooth performance to your devices.",
-  changelog: "https://telegra.ph/Flashing-Step-Notes-Changelog-Known-Bugs-04-03",
+  changelog: "https://telegra.ph/Flashing-Step-Notes-Changelog-Known-Bugs-05-10",
   guideFile: "guides/nothing-os.md",
   downloads: [
+    {
+      group: "Stable-Release",
+      items: [
+        { name: "Nothing OS 4.0 - Stable-Release", 
+          version: "X1.2", tag: "Stable", 
+          device: "Infinix GT 10 Pro", 
+          meta: "Android 16 . 3.12 GB", 
+          date: "2026-05-10", 
+          url: "https://drive.google.com/file/d/1iwQajIWNIPf--_EA5edqF1nEUHwKcmlL/view?usp=drive_link", 
+         // url2: ""
+        }
+      ]
+    },
     {
       group: "Pre-Release",
       items: [
@@ -16,8 +29,7 @@ OS_DATA.push({
           device: "Infinix GT 10 Pro", 
           meta: "Android 16 . 3.68 GB", 
           date: "2026-04-04", 
-          url: "https://sfl.gl/R2re", 
-          url2: "https://sfl.gl/Xiz9eNR5"
+          url: "https://sfl.gl/R2re"
         }
       ]
     }
