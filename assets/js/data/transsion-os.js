98925d718f85e47bb57e6186ea9291465baa65b4
ryanistr <ridho.holy@gmail.com>
2026-08-01 13:24:24 +0700

    feat: add Transsion OS banner image and update release data

--- assets/js/data/transsion-os.js
+++ assets/js/data/transsion-os.js
@@ -2,19 +2,26 @@ OS_DATA.push({
   id: "transsion-os",
   name: "Transsion OS",
   hide: false,
-  image: "assets/images/banners/hios.jpg",
+  image: "assets/images/banners/xos.jpg",
   shortDesc: "Transsion ecosystem port (HiOS/XOS).",
   fullDesc: "Ported firmware from the Transsion ecosystem, featuring unique system apps and UI components.",
-  changelog: "https://telegra.ph/Flashing-Step-Notes-Changelog-Known-Bugs-09-30",
+  changelog: "https://telegra.ph/Flashing-Step-Notes-Changelog-Known-Bugs-08-01-2",
   guideFile: "guides/transsion-os.md",
   downloads: [
     {
+      group: "XOS Stable Releases",
+      items: [
+        { name: "XOS 16.2.0 - Core", version: "X1", tag: "stable", device: "Infinix GT 10 Pro", meta: "Android 16 . 7.5 GB", date: "2026-08-02", url: "https://sfl.gl/w9x5"},
+        { name: "XOS 16.2.0 - PIF", version: "X1", tag: "stable", device: "Infinix GT 10 Pro", meta: "Android 16 . 7.5 GB", date: "2026-08-02", url: "https://sfl.gl/R0fe8iS"}
+      ]
+    },
+    {
       group: "HiOS Stable Releases",
       items: [
-        { name: "HiOS 15.1.0 - Stable", version: "X1", tag: "stable", device: "Infinix GT 10 Pro", meta: "Android 15 . 5.72 GB", date: "2025-10-03", url: "https://sfl.gl/M6hYob3" },
-        { name: "HiOS 15.1.0 - Stable", version: "X1", tag: "stable", device: "Infinix Zero 30 5G", meta: "Android 15 . 5.49 GB", date: "2025-10-09", url: "https://sfl.gl/fOq2Xci" },
-        { name: "HiOS 15.1.0 - Stable", version: "X1", tag: "stable", device: "Tecno Camon 20 Pro 5G", meta: "Android 15 . 5.71 GB", date: "2025-10-03", url: "https://sfl.gl/OhcO" },
-        { name: "HiOS 15.1.0 - Stable", version: "X1", tag: "stable", device: "Tecno Camon 20s Pro 5G", meta: "Android 15 . 5.71 GB", date: "2025-10-03", url: "https://sfl.gl/bM2Xw" }
+        { name: "HiOS 15.1.0 ", version: "X1", tag: "stable", device: "Infinix GT 10 Pro", meta: "Android 15 . 5.72 GB", date: "2025-10-03", url: "https://sfl.gl/M6hYob3" },
+        { name: "HiOS 15.1.0 ", version: "X1", tag: "stable", device: "Infinix Zero 30 5G", meta: "Android 15 . 5.49 GB", date: "2025-10-09", url: "https://sfl.gl/fOq2Xci" },
+        { name: "HiOS 15.1.0 ", version: "X1", tag: "stable", device: "Tecno Camon 20 Pro 5G", meta: "Android 15 . 5.71 GB", date: "2025-10-03", url: "https://sfl.gl/OhcO" },
+        { name: "HiOS 15.1.0 ", version: "X1", tag: "stable", device: "Tecno Camon 20s Pro 5G", meta: "Android 15 . 5.71 GB", date: "2025-10-03", url: "https://sfl.gl/bM2Xw" }
       ]
     }
   ]
